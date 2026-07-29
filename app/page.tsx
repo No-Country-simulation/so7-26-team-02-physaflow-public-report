d45efb8b9a92554914f031545a4f905d8661fb23
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2026-07-29 13:24:22 -0500

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PaletteTest from "./components/PaletteTest";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-background h-screen">
+    <div className="flex flex-col flex-1 items-center justify-center bg-background">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16">
         <PaletteTest />
       </main>
